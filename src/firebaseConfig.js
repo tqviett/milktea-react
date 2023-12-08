a590257b3b60f177e48a220110d8bfc6b90360c2
Viettq <taquocviet262@gmail.com>
2023-12-08 22:54:45 +0700

    update key

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,14 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAn7lruDLRIpBHV7MtwNklCJW0ibrQOvRU",
-  authDomain: "milkteashop-tunztunzz.firebaseapp.com",
-  databaseURL: "https://milkteashop-tunztunzz-default-rtdb.firebaseio.com",
-  projectId: "milkteashop-tunztunzz",
-  storageBucket: "milkteashop-tunztunzz.appspot.com",
-  messagingSenderId: "831318455172",
-  appId: "1:831318455172:web:7e9fc51e9032e427d823d1",
-  measurementId: "G-ZC2LD9K4YV"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
