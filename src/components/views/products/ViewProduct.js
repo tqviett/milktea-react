9bf4f72bb4ad001216a788373783fdcaffb39f84
Viettq <taquocviet262@gmail.com>
2023-12-09 21:04:53 +0700

    update viewproduct

--- src/components/views/products/ViewProduct.js
+++ src/components/views/products/ViewProduct.js
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import React, { useState, useEffect } from "react";
 import app from "../../../firebaseConfig";
 import { getDatabase, ref, get, remove } from "firebase/database";
 import { useNavigate } from "react-router-dom";
@@ -6,39 +6,39 @@ import { useNavigate } from "react-router-dom";
 function UpdateViewProduct() {
   const navigate = useNavigate();
 
-  let [drinkArray, setdrinkArray] = useState([]);
+  const [drinkArray, setdrinkArray] = useState([]);
 
   const fetchData = async () => {
     const db = getDatabase(app);
     const dbRef = ref(db, "drinks/");
     const snapshot = await get(dbRef);
     if (snapshot.exists()) {
-
       const myData = snapshot.val();
-      const temporaryData = Object.keys(myData).map(myFireId =>{
-         return {
-            ...myData[myFireId],
-            drinksid: myFireId
-         }
-      });
+      const temporaryData = Object.keys(myData).map((myFireId) => ({
+        ...myData[myFireId],
+        drinksid: myFireId,
+      }));
 
       setdrinkArray(temporaryData);
     } else {
       alert("error");
     }
   };
-  
-  const deleteDrink= async (drinksIdParam) => {
+
+  const deleteDrink = async (drinksIdParam) => {
     const db = getDatabase(app);
-    const dbRef = ref(db, "drinks/"+ drinksIdParam);
+    const dbRef = ref(db, "drinks/" + drinksIdParam);
     await remove(dbRef);
     window.location.reload();
-  }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []); // Empty dependency array means this effect will run once when the component mounts
 
   return (
     <div>
       <h1> View Product</h1>
-      <button onClick={fetchData}> Display Data </button>
       <ul>
         {drinkArray.map((item, index) => (
           <li key={index}>
@@ -47,13 +47,14 @@ function UpdateViewProduct() {
             {item.drinks_image}:{item.price}:
             {item.rating}:{item.sale}:
             {item.sold_count}:{item.status}
-            <button onClick={()=> navigate(`/update-product/${item.drinksid}`)}>UPDATE</button>
-            <button onClick={()=> deleteDrink(item.drinksid)}>DELETE</button>
-
+            <button onClick={() => navigate(`/update-product/${item.drinksid}`)}>
+              UPDATE
+            </button>
+            <button onClick={() => deleteDrink(item.drinksid)}>DELETE</button>
           </li>
         ))}
       </ul>
-      
+
       <button className="button1" onClick={() => navigate("/create-new-product")}>
         GO ADD PRODUCT
       </button>
